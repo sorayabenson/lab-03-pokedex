910619893d94dca5296a6e698c2b3c30cc4d12f6
Soraya Benson <soraya.benson@gmail.com>
2021-02-12 01:05:18 -0800

    link on home elements for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import './Home.css';
 import Header from './Header.js';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className="home">
-        <Header />
+        <Header className="header"/>
         <main className="homeMain">
-            <img src="https://i.pinimg.com/originals/66/89/dc/6689dc331be27e66349ce9a4d15ddff3.gif" alt="running pikachu" />
-            <h1>gotta catch them all!</h1>
+            <Link to="/search" className="link">
+              <img src="https://i.pinimg.com/originals/66/89/dc/6689dc331be27e66349ce9a4d15ddff3.gif" alt="running pikachu" />
+              <h1>gotta catch them all!</h1>
+            </Link>
         </main>
     </div>
   );
